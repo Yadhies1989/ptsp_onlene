0733f554ca5b3c92cabd176e788fe312db865c56
yadhies1989 <yadi.sepriadi@gmail.com>
2023-02-16 08:56:17 +0700

    second

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -1,28 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Beranda extends CI_Controller {
+class Beranda extends CI_Controller
+{
 
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->library('form_validation');
-
-		
 	}
 
 	public function index()
 	{
-		$data['title']   = 'PTSP PA Bojonegoro' ;
-		$data['about']   = 'about' ;
-		$data['clients'] = 'clients' ;
-		$data['team']    = 'team' ;
-		$data['service'] = 'services' ;
-		
+		$data['title']   = 'PTSP PA Bojonegoro';
+		$data['about']   = 'about';
+		$data['clients'] = 'clients';
+		$data['team']    = 'team';
+		$data['service'] = 'services';
+
 		$this->load->view('frontend/header', $data);
-        $this->load->view('frontend/kontent');
-        $this->load->view('frontend/footer');
-		
-		
+		$this->load->view('frontend/kontent');
+		$this->load->view('frontend/footer');
 	}
 }
